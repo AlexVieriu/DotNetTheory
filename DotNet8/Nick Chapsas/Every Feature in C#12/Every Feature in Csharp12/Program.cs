c1d14e82d389fc185ac653b85629f76cfffe6de2
AlexVieriu <vieriu.alexandru.25@gmail.com>
2023-11-15 11:21:40 +0200

    Adding new projects

--- DotNet8/Nick Chapsas/Every Feature in C#12/Every Feature in Csharp12/Program.cs
+++ DotNet8/Nick Chapsas/Every Feature in C#12/Every Feature in Csharp12/Program.cs
@@ -1,16 +1,38 @@
-﻿// Primary Constructors
-public class Person(string name) // only a default constructor, but it can change
-{
-    // can change into private filed(NOT REALDONLY- we can still change it) if u write it like this
-    public void Test()
-    {
-        name = "";
-        Console.WriteLine(name);
-    }
-}
+﻿//// 1. Primary Constructors
+//public class Person(string name) // only a default constructor, but it can change
+//{
+//    // can change into private filed(NOT REALDONLY- we can still change it) if u write it like this
+//    public void Test()
+//    {
+//        name = "";
+//        Console.WriteLine(name);
+//    }
+//}
 
-// Collection Expression
+
+//// 2. Collection Expression
+
+//int[] a = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }; // C#11
+//int[] b = { 1, 2, 3, 4, 5, 6, 7, 8 };           // C#12
+//// var a = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }; // doesn't work
+
+//Span<int> c = ['a', 'b', 'c', 'd'];
+
+//// jagged array
+//int[][] twoD = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+//int[] row0 = [1, 2, 3];
+//int[] row1 = [1, 2, 3];
+//int[] row2 = [1, 2, 3];
+//int[][] twoDFromVariables = [row0, row1, row2];
+
+//Dictionary<string, int> dic1 = new();   // C#11
+//Dictionary<string, int> dic2 = [];      // C#12
+
+
 // Ref Readonly Parameters
+
+
 // Default Lambda Parameters
 // Alias Any Type
 // Inline Arrays
