ed921bb3eac3c2fd01a9adb17a633e4cf7c82c16
AlexVieriu <vieriu.alexandru.25@gmail.com>
2023-12-21 15:22:01 +0200

    15_Configuration in Depth

---
+++ DotNet8/dotNetConf/15_Configuration in Depth/FileOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Configuration_in_Depth;
+
+public class FileOptions
+{
+    public string FileExtension { get; set; } = "";
+    public string OutputDir { get; set; } = "";
+    public string TemplateFile { get; set; } = "";
+}
---
+++ DotNet8/dotNetConf/15_Configuration in Depth/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Spectre.Console;
+using static System.Console;
+
+// order matters
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddJsonFile("config.json")
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>()
+    .AddCommandLine(args)
+    .Build();
+
+// bash
+// dotnet run --environment=prod
+// environment=qa dotnet run
+// environment=qa dotnet run --environment=prod
+// greeting=hey dotnet run --environment=prod
+
+var name = "World";
+
+//dotnet run --greeting:color="blue"
+
+//AnsiConsole.MarkupLine($"[{configuration["greeting:color"]}]{configuration["greeting:message"]}, {name}![/]");
+WriteLine($"{config["greeting"]}, {name}!");
+WriteLine($"Configuration: {config["environment"]}");
+
