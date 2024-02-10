3e35aba5912bfa61afdb70d906218f04ad60caa7
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-02-10 07:00:32 +0200

    CodeCop10_Loops

---
+++ DotNet8/Nick Chapsas/CodeCop010/Benchmarks.cs
@@ -0,0 +1,47 @@
+﻿namespace CodeCop010;
+
+using BenchmarkDotNet.Attributes;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    private int[] _array = null!;
+
+    [Params(100, 10_000, 100_000)]
+    public int Numbers { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _array = Enumerable.Range(0, Numbers).ToArray();
+    }
+
+    [Benchmark]
+    public long CountNormalLoop()
+    {
+        var count = 0L;
+        var length = _array.Length;
+        for (int i = 0; i < length; i++)
+            count += _array[i];
+
+        return count;
+    }
+
+    [Benchmark]
+    public long CountUnwoundLoop_5()
+    {
+        var count = 0L;
+        var length = _array.Length;
+
+        for (int i = 0; i < length; i += 5)
+        {
+            count += _array[i];
+            count += _array[i + 1];
+            count += _array[i + 2];
+            count += _array[i + 3];
+            count += _array[i + 4];
+        }
+
+        return count;
+    }
+}
--- DotNet8/Nick Chapsas/CodeCop010/Program.cs
+++ DotNet8/Nick Chapsas/CodeCop010/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// "Don't Use Loops, They Are Slow! Do This Instead" | Code Cop 10
+// https://www.youtube.com/watch?v=tllygkj0czw&list=TLPQMTAwMjIwMjT9By1a00n_kw&index=1&ab_channel=NickChapsas
+
+using BenchmarkDotNet.Running;
+using CodeCop010;
+
+BenchmarkRunner.Run<Benchmarks>();
