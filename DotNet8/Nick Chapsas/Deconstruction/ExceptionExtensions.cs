dbeb5603678cb187a3382e2dd1e113fac370ae36
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-02-11 09:35:21 +0200

    Deconstruction

---
+++ DotNet8/Nick Chapsas/Deconstruction/ExceptionExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Deconstruction;
+
+public static class ExceptionExtensions
+{
+    // the name of the method is really important 
+    public static void Deconstruct(this Exception ex, out string message, out string? stackTrace)
+    {
+        message = ex.Message;
+        stackTrace = ex.StackTrace;
+    }
+}
---
+++ DotNet8/Nick Chapsas/Deconstruction/Program.cs
@@ -0,0 +1,10 @@
+﻿using Deconstruction;
+
+var keyValuePair = new KeyValuePair<string, int>("Nick", 69);
+var (key1, value) = new KeyValuePair<string, int>("Nick", 69);
+var (key2, _) = new KeyValuePair<string, int>("Nick", 69);
+
+// check the Datetime class and look at the Deconstruct properties
+var (year, month, day) = new DateTime();
+
+var (message, stackTrace) = new Exception();
